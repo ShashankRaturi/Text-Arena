cd65ad7ab6da4b049789d173140e7f1ff69ccffc
shashank <shashankraturi4@gmail.com>
2023-03-05 12:09:07 +0530

    Updates

---
+++ precache-manifest.722d99824a9a58db82e84ac53cf83d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c22f8daa81f3df6a3b5d",
+    "url": "/Text-Arena/static/css/main.d4aad477.chunk.css"
+  },
+  {
+    "revision": "c22f8daa81f3df6a3b5d",
+    "url": "/Text-Arena/static/js/main.c22f8daa.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Text-Arena/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text-Arena/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8ebec372807cb7c0193a",
+    "url": "/Text-Arena/static/js/runtime~main.8ebec372.js"
+  },
+  {
+    "revision": "d82285f494b5a4a0df389e60b95d5282",
+    "url": "/Text-Arena/index.html"
+  }
+];
